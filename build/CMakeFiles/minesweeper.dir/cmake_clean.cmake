b06ff0e58e2b8d80c260c8290f5eda4743841337
XJacksonChrisX <164982911+XJacksonChrisX@users.noreply.github.com>
2026-04-22 18:30:44 +0000

    chore: sync local workspace changes

---
+++ build/CMakeFiles/minesweeper.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minesweeper.dir/src/board.cpp.o"
+  "CMakeFiles/minesweeper.dir/src/board.cpp.o.d"
+  "CMakeFiles/minesweeper.dir/src/cell.cpp.o"
+  "CMakeFiles/minesweeper.dir/src/cell.cpp.o.d"
+  "CMakeFiles/minesweeper.dir/src/game.cpp.o"
+  "CMakeFiles/minesweeper.dir/src/game.cpp.o.d"
+  "CMakeFiles/minesweeper.dir/src/main.cpp.o"
+  "CMakeFiles/minesweeper.dir/src/main.cpp.o.d"
+  "minesweeper"
+  "minesweeper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minesweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/minesweeper.dir/src/cell.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/minesweeper.dir/src/cell.cpp.o: /workspaces/game/src/cell.cpp \
+ /usr/include/stdc-predef.h /workspaces/game/include/cell.h
